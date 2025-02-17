49303a72fec60343cb151cd9fef6a5902313cf0c
Aaron Abbott <aabmass@gmail.com>
2025-02-17 17:19:01 -0500

    Update script (#10)

--- update_samples.sh
+++ update_samples.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-pipenv run python -m cumulative_snow.main datasets/boston_logan_snowfall.csv --start_year=2010 --output_path=boston_logan.svg
-pipenv run python -m cumulative_snow.main datasets/boston_logan_snowfall.csv --start_year=2010 --output_path=boston_logan.pdf
+uv run main.py datasets/boston_logan_snowfall.csv --start_year=2010 --output_path=boston_logan.svg
+uv run main.py datasets/boston_logan_snowfall.csv --start_year=2010 --output_path=boston_logan.pdf
