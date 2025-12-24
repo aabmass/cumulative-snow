ba52157220a442271f170cd0efe10ae9d950da35
Aaron Abbott <aabmass@gmail.com>
2025-12-24 16:14:43 -0500

    Build in subdir

--- update_samples.sh
+++ update_samples.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-uv run main.py datasets/boston_logan_snowfall.csv --start_year=2010 --output_path=boston_logan.svg
-uv run main.py datasets/boston_logan_snowfall.csv --start_year=2010 --output_path=boston_logan.pdf
+uv run main.py datasets/boston_logan_snowfall.csv --start_year=2010 --output_path=www/boston.html
