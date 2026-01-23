4e8cbba9951118343602421b720c6459afad83bc
Aaron Abbott <aabmass@gmail.com>
2026-01-23 00:16:35 -0500

    Working with s3 loading

--- marimo_s3_parquet.py
+++ marimo_s3_parquet.py
@@ -1,13 +1,47 @@
 import marimo
 
-__generated_with = "0.19.5"
+__generated_with = "0.19.6"
 app = marimo.App(width="medium")
+
+
+@app.cell
+def _(mo, pl):
+    @mo.persistent_cache
+    def load_stations():
+        return (
+            # scan_csv doesn't work with WASM for some reason https://github.com/pola-rs/polars/issues/20877
+            pl.read_csv(
+                "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.csv",
+                has_header=False,
+                truncate_ragged_lines=True,
+                quote_char=None,
+                # See https://www.ncei.noaa.gov/pub/data/ghcn/daily/readme.txt for details
+                schema={
+                    "ID": pl.String,
+                    "LATITUDE": pl.Float32,
+                    "LONGITUDE": pl.Float32,
+                    "ELEVATION": pl.Float32,
+                    "STATE": pl.String,
+                    "NAME": pl.String,
+                    "GSN_FLAG": pl.String,
+                    "HCN_CRN_FLAG": pl.String,
+                    "WMO_ID": pl.Int64,
+                },
+            ).filter(
+                pl.col("ID").str.starts_with("US"),
+            )
+        )
+
+
+    stations_df = load_stations()
+    stations_df
+    return (stations_df,)
 
 
 @app.cell
 def _(data, mo):
     _stations = (
-        data.groupby(["STATION", "NAME"])
+        data.groupby(["ID"])
         .agg(SNOW=("SNOW", "count"), DATE_min=("DATE", "min"), DATE_max=("DATE", "max"))
         .sort_values(by="SNOW", ascending=False)
         .rename({"SNOW": "Number of Datapoints"}, axis="columns")
@@ -35,19 +69,66 @@ def _(data, mo, plot):
 
 
 @app.cell
-def _(Args, mo):
-    args = Args(csv_path=mo.notebook_dir() / "datasets" / "boston_logan_snowfall.csv")
-    return (args,)
+def _(mo, stations_df):
+    station_dropdown = mo.ui.dropdown(
+        {
+            f"{state} - {name}": id
+            for name, state, id in zip(
+                stations_df["NAME"], stations_df["STATE"], stations_df["ID"]
+            )
+        },
+        label="Choose a station",
+    )
+    station_dropdown
+    return (station_dropdown,)
 
 
 @app.cell
-def _(args, load_data):
-    data = load_data.load_noaa_data(args)
+def _(mo, station_dropdown):
+    duckdb_df = mo.sql(
+        f"""
+        INSTALL httpfs;
+        LOAD httpfs;
+
+        PIVOT (
+            SELECT
+                ID,
+                strptime(DATE, '%Y%m%d')::DATE AS DATE,
+                ELEMENT,
+                CASE
+                -- Temperature: Tenths of C to Fahrenheit
+                    WHEN ELEMENT IN ('TMAX', 'TAVG', 'TMIN', 'TOBS') THEN ROUND((DATA_VALUE / 10.0) * 1.8 + 32, 2)
+                    -- Precipitation: Tenths of mm to Inches
+                    WHEN ELEMENT = 'PRCP' THEN ROUND(DATA_VALUE / 254.0, 2)
+                    -- Snow: mm to Inches
+                    WHEN ELEMENT IN ('SNOW', 'SNWD') THEN ROUND(DATA_VALUE / 25.4, 2)
+                    ELSE DATA_VALUE
+                END AS converted_value
+            FROM
+                read_parquet(
+                    's3://noaa-ghcn-pds/parquet/by_station/STATION={station_dropdown.value}/ELEMENT=*/*.parquet',
+                    hive_partitioning = true
+                )
+            WHERE
+                ELEMENT IN ('TMAX', 'TAVG', 'TMIN', 'PRCP', 'SNOW', 'SNWD')
+                AND YEAR(strptime(DATE, '%Y%m%d')) > 1990
+        ) ON ELEMENT IN ('TMAX', 'TAVG', 'TMIN', 'PRCP', 'SNOW', 'SNWD') USING FIRST(converted_value)
+        GROUP BY
+            ID,
+            DATE
+        """
+    )
+    return (duckdb_df,)
+
+
+@app.cell
+def _(duckdb_df, load_data):
+    data = load_data.load_noaa_data(duckdb_df.to_pandas())
     return (data,)
 
 
 @app.cell
-async def _(is_wasm, mo):
+async def _(mo):
     if is_wasm():
         import micropip
 
@@ -58,12 +139,14 @@ async def _(is_wasm, mo):
     import plotly.express as px
     import plotly.io as pio
 
+    import polars as pl
+
     from cumulative_snow import load_data, plot
     from cumulative_snow.args import Args
 
     # Set plotly theme based on marimo theme
     pio.templates.default = "plotly_dark" if mo.app_meta().theme == "dark" else None
-    return Args, load_data, plot
+    return load_data, pl, plot
 
 
 @app.cell
@@ -72,16 +155,11 @@ def _():
     return (mo,)
 
 
-@app.cell
-def _():
-    def is_wasm():
-        import sys
+@app.function
+def is_wasm():
+    import sys
 
-        return "pyodide" in sys.modules
-
-
-    is_wasm()
-    return (is_wasm,)
+    return "pyodide" in sys.modules
 
 
 if __name__ == "__main__":
--- src/cumulative_snow/load_data.py
+++ src/cumulative_snow/load_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 from cumulative_snow.args import Args
 
 
-def load_noaa_data(args: Args) -> pd.DataFrame:
+def load_noaa_data(data: pd.DataFrame) -> pd.DataFrame:
     """Loads NOAA data from file path.
 
     Output dataframe has columns:
@@ -17,16 +17,16 @@ def load_noaa_data(args: Args) -> pd.DataFrame:
 
     Additional dataset documentation: https://bit.ly/2Rs3Xyb
     """
-    data = pd.read_csv(
-        args.csv_path,
-        converters={
-            "SNOW": _fill_zeroes,
-            "SNWD": _fill_zeroes,
-            "WESD": _fill_zeroes,
-            "WESF": _fill_zeroes,
-        },
-        parse_dates=["DATE"],
-    )
+    # data = pd.read_csv(
+    #     args.csv_path,
+    #     converters={
+    #         "SNOW": _fill_zeroes,
+    #         "SNWD": _fill_zeroes,
+    #         "WESD": _fill_zeroes,
+    #         "WESF": _fill_zeroes,
+    #     },
+    #     parse_dates=["DATE"],
+    # )
 
     # Assigns year periods as being from July to the end of June e.g. winter of
     # 2010 is July 2009 through June 2019
@@ -38,9 +38,6 @@ def load_noaa_data(args: Args) -> pd.DataFrame:
 
     # Cumulative snow for the year
     data["CUMULATIVE_SNOW"] = data.groupby("WINTER_YEAR")["SNOW"].cumsum()
-
-    if args.station:
-        data = data[data["STATION"] == args.station]
 
     return data
 
